fa8d6930ceabc02023bdce6875da1e25a6ae7830
Szczepan <szczep.kowalik@gmail.com>
2022-10-03 15:51:53 +0200

    Add project files.

---
+++ Xml_Task/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Xml_Task.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
