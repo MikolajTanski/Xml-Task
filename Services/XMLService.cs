ee4a5cbdd42edc82e2d98d26c1146c39d95d7211
Szczepan <szczep.kowalik@gmail.com>
2022-10-16 10:34:55 +0200

    XML to User class method

---
+++ Services/XMLService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using Xml_Task.Models;
+
+namespace Xml_Task.Services
+{
+    public class XMLService
+    {
+        public void Save(string filePath)
+        {
+            User user = GetUserFromXML(filePath);
+            string name = user.Name;
+
+            //File.Delete(filePath);
+        }
+        internal User GetUserFromXML(string filePath)
+        {
+            User objectToDeserialize = new User();
+            XmlSerializer xmlserializer = new System.Xml.Serialization.XmlSerializer(objectToDeserialize.GetType());
+
+            //Use using so that streamReader object will be cleaned up properly after use. 
+            using (StreamReader streamReader = new StreamReader(filePath))
+            {
+                return (User)xmlserializer.Deserialize(streamReader);
+            }
+        }
+    }
+}
