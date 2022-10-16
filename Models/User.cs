cb4c6f779dde76ce610ae7e3755871b8bbc0de10
Szczepan <szczep.kowalik@gmail.com>
2022-10-16 10:34:13 +0200

    Added user model

---
+++ Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace Xml_Task.Models
+{
+
+    [Serializable, XmlRoot("user")]
+    public class User
+    {
+        [XmlElement(ElementName = "id")]
+        public int Id { get; set; }
+        [XmlElement(ElementName = "name")]
+        public string Name { get; set; }
+        [XmlElement(ElementName = "email")]
+        public string Email { get; set; }
+
+        [XmlElement(ElementName = "age")]
+        public int Age  { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}\n Name: {Name}\n Email: {Email}\n Age: {Age}";
+        }
+    }
+}
